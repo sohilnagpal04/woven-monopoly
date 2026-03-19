e5cd8fb1b4f2c9a0e7bc5a3528ec7c7764ff8ec1
Sohil Nagpal <sohil4793.be21@chitkara.edu.in>
2026-03-19 16:34:59 +1100

    Add Player class with movement, buying and rent logic

---
+++ src/player.py
@@ -0,0 +1,24 @@
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.money = 16
+        self.position = 0
+        self.properties = []
+
+    def move(self, steps, board_size):
+        old_position = self.position
+        self.position = (self.position + steps) % board_size
+        passed_go = self.position < old_position
+        return passed_go
+
+    def buy_property(self, property):
+        self.money -= property.price
+        self.properties.append(property)
+        property.owner = self
+
+    def pay_rent(self, amount, owner):
+        self.money -= amount
+        owner.money += amount
+
+    def is_bankrupt(self):
+        return self.money < 0
