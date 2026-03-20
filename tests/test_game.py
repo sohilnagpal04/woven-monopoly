1130d300575d738f55a4d8babd70a984128b12d7
Sohil Nagpal <sohil4793.be21@chitkara.edu.in>
2026-03-20 16:11:41 +1100

    test: verify initial player state

--- tests/test_game.py
+++ tests/test_game.py
@@ -16,3 +16,14 @@ def test_game_runs_without_crashing():
     game.play()
 
     assert len(game.players) == 4
+
+#Test 2 : Players start correctly at position 0 with $16 and 4 players
+def test_initial_player_state():
+    board = Board("data/board.json")
+    rolls = load_rolls("data/rolls_1.json")
+
+    game = Game(board, rolls)
+
+    for player in game.players:
+        assert player.money == 16
+        assert player.position == 0
